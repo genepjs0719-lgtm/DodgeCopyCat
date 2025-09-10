8e460517e66fe93ce06f4f488fb1f8fdf5c0b6f8
genepjs0719-lgtm <genepjs0719@gmail.com>
2025-09-10 10:23:20 +0900

    202509101022_ini
    깃허브 연동 확인 테스트

---
+++ Assets/GitHubTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GitHubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        깃허브 첨 사용해봐. 연동이 잘 되면 좋겠어
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
