f26066b1f9d255bd852a347ca22eaad6f6d30bdc
genepjs0719-lgtm <genepjs0719@gmail.com>
2025-09-10 10:02:35 +0900

    Initial commit

---
+++ Assets/03. Script/Bullet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 6f;
+    private Rigidbody bulletRigidbody;
+
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+
+        bulletRigidbody.linearVelocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+
+
+}
---
+++ Assets/03. Script/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+
+
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xspeed = xInput * speed;
+        float zspeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xspeed, 0f, zspeed);
+        playerRigidbody.linearVelocity = newVelocity;
+
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+
+       
+    }
+}
